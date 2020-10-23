7c5e6873c4e8d48557ff50fa5b11bd7eabf1fd91
Fer Valdez <f.douzez@gmail.com>
2020-10-23 03:13:16 -0400

    add blog app and its main structure

--- fercodes/settings.py
+++ fercodes/settings.py
@@ -21,6 +21,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
